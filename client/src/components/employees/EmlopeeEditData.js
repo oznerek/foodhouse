1c4207cd5f1a1512bd8a8c05d47b97355a36a61c
oznerek <oznerek@gmail.com>
2019-07-16 21:44:51 +0200

    .

--- client/src/components/employees/EmlopeeEditData.js
+++ client/src/components/employees/EmlopeeEditData.js
@@ -1,59 +1,64 @@
 import React from "react";
-import $ from 'jquery';
+import $ from "jquery";
 
 class EmployeeEditData extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      category : 'profile',
-      validation: 'What is wrong',
-      profile: ['Change name', 'Change last name', 'Login'],
-      email:  ['Old email', 'New email', 'Confirm new email'],
-      password: ['Old password', 'New password', 'Confirm new password']
-    }
+      category: "profile",
+      validation: "What is wrong",
+      profile: ["Change name", "Change last name", "Login"],
+      email: ["Old email", "New email", "Confirm new email"],
+      password: ["Old password", "New password", "Confirm new password"]
+    };
   }
   componentDidMount() {
-    this.changeColor()
+    this.changeColor();
   }
 
-selectCategory=this.selectCategory.bind(this);
-selectCategory(event) {
-   this.setState({category: event.target.getAttribute('name')})
-   this.changeColor();
-}
+  selectCategory = this.selectCategory.bind(this);
+  selectCategory(event) {
+    this.setState({ category: event.target.getAttribute("name") });
+    this.changeColor();
+  }
 
-changeColor() {
-  $(".edit__navigation-item").click(function() {
-    $(".edit__navigation-item").removeClass("editActive");
-    $(this)
-      .closest(".edit__navigation-item")
-      .addClass("editActive");
-  })
-}
+  changeColor() {
+    $(".edit__navigation-item").click(function() {
+      $(".edit__navigation-item").removeClass("editActive");
+      $(this)
+        .closest(".edit__navigation-item")
+        .addClass("editActive");
+    });
+  }
 
   renderView() {
     let name = this.state.category;
-    let cos = `this.state.`+ name
-    console.log(cos);
+    let fieldName = "";
+    if (name === "profile") {
+      fieldName = this.state.profile;
+    } else if (name === "email") {
+      fieldName = this.state.email;
+    } else if (name === "password") {
+      fieldName = this.state.password;
+    }
 
     return (
-      <div> 
+      <div>
         <form className="profile__form">
-          
-          <div className='profile__validation'>{this.state.validation}</div>
+          <div className="profile__validation">{this.state.validation}</div>
           <div>
-            <label className="profile__label"> {this.state.profile[0]}</label>
+            <label className="profile__label"> {fieldName[0]}</label>
             <input className="profile__input" />
           </div>
           <div>
-            <label className="profile__label"> {this.state.profile[1]}</label>
+            <label className="profile__label"> {fieldName[1]}</label>
             <input className="profile__input" />
           </div>
           <div>
-            <label className="profile__label">{this.state.profile[2]}</label>
+            <label className="profile__label">{fieldName[2]}</label>
             <input className="profile__input" />
-          </div> 
-
+          </div>
+          <label className='profile__label'></label>
           <button className="btn profile__btn">Save data</button>
         </form>
       </div>
@@ -61,19 +66,30 @@ changeColor() {
   }
 
   render() {
-    console.log(this.state.category)
     return (
       <span className="edit">
         <nav className="edit">
           <ul className="edit__navigation">
-            <li className="edit__navigation-item editActive " name='profile' onClick={this.selectCategory}>
-                Profile
+            <li
+              className="edit__navigation-item editActive "
+              name="profile"
+              onClick={this.selectCategory}
+            >
+              Profile
             </li>
-            <li className="edit__navigation-item" name='email' onClick={this.selectCategory}>
-                Email
+            <li
+              className="edit__navigation-item"
+              name="email"
+              onClick={this.selectCategory}
+            >
+              Email
             </li>
-            <li className="edit__navigation-item" name='password' onClick={this.selectCategory}>
-                Password
+            <li
+              className="edit__navigation-item"
+              name="password"
+              onClick={this.selectCategory}
+            >
+              Password
             </li>
           </ul>
         </nav>
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -3,6 +3,8 @@ import React from "react";
 import CommentCards from "./commentsCard";
 import Header from "./Header";
 import Navigation from "./Navigation";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
 
 class Home extends React.Component {
   constructor(props) {
@@ -50,7 +52,6 @@ class Home extends React.Component {
       });
   }
 
- 
   randomComments() {
     let commentsNumber = Math.floor(
       Math.random() * this.state.commentsList.length
@@ -97,125 +98,138 @@ class Home extends React.Component {
           </div>
 
           {/* -- CHEFS PARTS ----------------------------- */}
-          <div id="chefs" className="chefs">
-            <div className="chefs__left">
-              <img src="img/chefs.jpg" alt="chefs" className="chefs__img" />
+          <Slide bottom>
+            <div id="chefs" className="chefs">
+              <div className="chefs__left">
+                <img src="img/chefs.jpg" alt="chefs" className="chefs__img" />
+              </div>
+              <div className="chefs__container">
+                <h1 className="chefs__header">The Best Chefs</h1>
+                <Fade right>
+                  <div className="chefs__card">
+                    <div className="chefs__user">
+                      <img
+                        src="img/vietnam.jpg"
+                        alt="Thien_Lou"
+                        className="chefs__user-photo"
+                      />
+                    </div>
+                    <div className="chefs__data">
+                      <h2 className="chefs__name">Thien Lou</h2>
+                      <p className="chefs__description">
+                        Master Chefs in orietnal cousine
+                      </p>
+                    </div>
+                  </div>
+                  <div className="chefs__card">
+                    <div className="chefs__user">
+                      <img
+                        src="img/france.jpg"
+                        alt="Olympe de Beauvoir"
+                        className="chefs__user-photo"
+                      />
+                    </div>
+                    <div className="chefs__data">
+                      <h2 className="chefs__name">Olympe de Beauvoir</h2>
+                      <p className="chefs__description">
+                        French winner of the hell's kitchen program
+                      </p>
+                    </div>
+                  </div>
+                  <div className="chefs__card">
+                    <div className="chefs__user">
+                      <img
+                        src="img/japan.jpg"
+                        alt="Ito Tanaka"
+                        className="chefs__user-photo"
+                      />
+                    </div>
+                    <div className="chefs__data">
+                      <h2 className="chefs__name">Ito Tanaka</h2>
+                      <p className="chefs__description">
+                        Best Sushi Cheft from Tokyo
+                      </p>
+                    </div>
+                  </div>
+                </Fade>
+              </div>
             </div>
-            <div className="chefs__container">
-              <h1 className="chefs__header">The Best Chefs</h1>
-              {/* <Fade left cascade> */}
-              <div className="chefs__card">
-                <div className="chefs__user">
-                  <img
-                    src="img/vietnam.jpg"
-                    alt="Thien_Lou"
-                    className="chefs__user-photo"
-                  />
-                </div>
-                <div className="chefs__data">
-                  <h2 className="chefs__name">Thien Lou</h2>
-                  <p className="chefs__description">
-                    Master Chefs in orietnal cousine
-                  </p>
-                </div>
-              </div>
-              <div className="chefs__card">
-                <div className="chefs__user">
-                  <img
-                    src="img/france.jpg"
-                    alt="Olympe de Beauvoir"
-                    className="chefs__user-photo"
-                  />
-                </div>
-                <div className="chefs__data">
-                  <h2 className="chefs__name">Olympe de Beauvoir</h2>
-                  <p className="chefs__description">
-                    French winner of the hell's kitchen program
-                  </p>
-                </div>
-              </div>
-              <div className="chefs__card">
-                <div className="chefs__user">
-                  <img
-                    src="img/japan.jpg"
-                    alt="Ito Tanaka"
-                    className="chefs__user-photo"
-                  />
-                </div>
-                <div className="chefs__data">
-                  <h2 className="chefs__name">Ito Tanaka</h2>
-                  <p className="chefs__description">
-                    Best Sushi Cheft from Tokyo
-                  </p>
-                </div>
-              </div>
-              {/* </Fade> */}
-            </div>
-          </div>
-          {/* ------- DESIGN -------------- */}
-          <div id="design" className="design">
-            <div className="design__shadow">
-              <div className="design__shadow-1" />
-              <div className="design__shadow-2" />
-              <div className="reserve">
-                <h2>· Buisness meetings</h2>
-                <h2>· Getaway with friends</h2>
-                <h2>· Romantic dinner for Two</h2>
-
-                <a href="/contact" className="btn reserve__btn">
+            {/* ------- DESIGN -------------- */}
+          </Slide>
+          <Slide bottom>
+            <div id="design" className="design">
+              <div className="design__shadow">
+                <Fade right>
                   {" "}
-                  Book Table Now
+                  <div className="design__shadow-1" />
+                </Fade>
+                <Fade left>
+                  <div className="design__shadow-2" />
+                </Fade>
+                <div className="reserve">
+                  <h2>· Buisness meetings</h2>
+                  <h2>· Getaway with friends</h2>
+                  <h2>· Romantic dinner for Two</h2>
+
+                  <a href="/contact" className="btn reserve__btn">
+                    {" "}
+                    Book Table Now
+                  </a>
+                </div>
+              </div>
+              <div className="design__secondimage" />
+            </div>
+            {/* ---- SOCIAL MEDIA --------------- */}
+          </Slide>
+          <Slide bottom>
+            <div id="socialmedia" className="socialmedia">
+              <h3 className="socialmedia__header">
+                Over 1500 satisfied customers
+              </h3>
+              <div className="socialmedia__container">
+                <div className="socialmedia__commentsCard">
+                  <Fade left>
+                    {this.randomComments()}
+                    {this.randomComments()}
+                  </Fade>
+                </div>
+                <div className="socialmedia__content">
+                  <img
+                    src="img/customers.jpg"
+                    className="socialmedia__photo"
+                    alt="Happy customers"
+                  />
+                </div>
+              </div>
+
+              <div className="socialmedia__links">
+                <a
+                  href="http://facebook.com"
+                  className="socialmedia__links-facebook"
+                >
+                  <i className="fab fa-facebook-square" />
+                </a>
+                <a
+                  href="http://instagram.com"
+                  className="socialmedia__links-instagram"
+                >
+                  <i className="fab fa-instagram" />
+                </a>
+                <a
+                  href="http:///twitter.com"
+                  className="socialmedia__links-twitter"
+                >
+                  <i className="fab fa-twitter-square" />
+                </a>
+                <a
+                  href="http://youtube.com"
+                  className="socialmedia__links-youtube"
+                >
+                  <i className="fab fa-youtube" />
                 </a>
               </div>
             </div>
-            <div className="design__secondimage" />
-          </div>
-          {/* ---- SOCIAL MEDIA --------------- */}
-          <div id="socialmedia" className="socialmedia">
-            <h3 className="socialmedia__header">
-              Over 1500 satisfied customers
-            </h3>
-            <div className="socialmedia__container">
-              <div className="socialmedia__commentsCard">
-                {this.randomComments()}
-                {this.randomComments()}
-              </div>
-              <div className="socialmedia__content">
-                <img
-                  src="img/customers.jpg"
-                  className="socialmedia__photo"
-                  alt="Happy customers"
-                />
-              </div>
-            </div>
-
-            <div className="socialmedia__links">
-              <a
-                href="http://facebook.com"
-                className="socialmedia__links-facebook"
-              >
-                <i className="fab fa-facebook-square" />
-              </a>
-              <a
-                href="http://instagram.com"
-                className="socialmedia__links-instagram"
-              >
-                <i className="fab fa-instagram" />
-              </a>
-              <a
-                href="http:///twitter.com"
-                className="socialmedia__links-twitter"
-              >
-                <i className="fab fa-twitter-square" />
-              </a>
-              <a
-                href="http://youtube.com"
-                className="socialmedia__links-youtube"
-              >
-                <i className="fab fa-youtube" />
-              </a>
-            </div>
-          </div>
+          </Slide>
         </section>
       </div>
     );
