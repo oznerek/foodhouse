b55111d068e8f376b3f6eba639efff8a41482b2a
oznerek <oznerek@gmail.com>
2019-08-07 11:44:20 +0200

    .

---
+++ client/build/precache-manifest.7385900dd7c007c5d5eb2535b1564f9f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eea9e88248d750924058",
+    "url": "/static/js/main.eea9e882.chunk.js"
+  },
+  {
+    "revision": "f2e40960248cb8e0295c",
+    "url": "/static/js/1.f2e40960.chunk.js"
+  },
+  {
+    "revision": "712d7b08011b5fce9662ab9802a22e11",
+    "url": "/index.html"
+  }
+];
