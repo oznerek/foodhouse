130c012ec17bcbc37291615951d5eddb64165365
oznerek <oznerek@gmail.com>
2019-08-07 11:48:10 +0200

    Updates

---
+++ precache-manifest.8157a6a6623ef6f91a32d86504158ee0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f07fa6b4c2f6e284d590",
+    "url": "/foodhouse/static/js/runtime~main.f07fa6b4.js"
+  },
+  {
+    "revision": "eea9e88248d750924058",
+    "url": "/foodhouse/static/js/main.eea9e882.chunk.js"
+  },
+  {
+    "revision": "f2e40960248cb8e0295c",
+    "url": "/foodhouse/static/js/1.f2e40960.chunk.js"
+  },
+  {
+    "revision": "67b31127922aac908a0ee3eae449e806",
+    "url": "/foodhouse/index.html"
+  }
+];
